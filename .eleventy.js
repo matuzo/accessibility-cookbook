95c842e28d7bb980730ad5aeb3a61a305ce690d8
Manuel Matuzovic <manuel@matuzo.at>
2024-06-19 10:31:59 +0200

    Update files

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,31 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPassthroughCopy("source/assets");
+  eleventyConfig.addPlugin(syntaxHighlight);
 
-  return {
-    dir: {
-      input: "source",
-      output: "demos"
+	eleventyConfig.addPassthroughCopy("images");
+	eleventyConfig.addPassthroughCopy("media");
+	eleventyConfig.addPassthroughCopy("base");
+	eleventyConfig.addPassthroughCopy("reset");
+	eleventyConfig.addPassthroughCopy("olivia.css");
+
+  eleventyConfig.addPassthroughCopy(
+    { 
+      images: "images",
+      'css/base': "assets/css/base",
+      'favicon.ico': "favicon.ico",
+      'robots.txt': "robots.txt",
+      'css/reset': "assets/css/reset",
+      'css/olivia.css': "assets/css/olivia.css",
+      'css/philippa.css': "assets/css/philippa.css",
+      'css/cookbook.css': "assets/css/cookbook.css",
+      'css/prism.css': "assets/css/prism.css",
+      'css/RedditSans-Variable.woff2': "assets/css/RedditSans-Variable.woff2",
+      'data/records.json': "assets/data/records.json"
     }
-  }
+  );
+
+  
 };
